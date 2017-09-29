0e0c82e3cd8962197dfbeee7feaede249986500f
Flannon Jackson <flannon@nyu.edu>
2017-09-29 13:20:46 -0400

    Cleaned up depricated comments

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,13 +27,9 @@ class archivesspace (
 
   ensure_resource('package', 'git', {'ensure' => 'present'})
 
-  #include archivesspace::database
   include archivesspace::install
   include archivesspace::service
-  #Class['archivesspace::database']->
   Class['archivesspace::install']->
   Class['archivesspace::service']
-
-
 
 }
